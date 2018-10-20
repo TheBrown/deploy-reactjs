b3476dcdd04bb15884eb63a01c2766a19fe1e9d8
Saleumsack <saleumsack16@gmail.com>
2018-10-20 15:04:01 +0700

    Updates

---
+++ precache-manifest.04bcb07cd12017aefe6dac30cb4fdc88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-reactjs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "02291bd6b0a4cde69ed0",
+    "url": "/deploy-reactjs/static/js/runtime~main.02291bd6.js"
+  },
+  {
+    "revision": "07cc854dca6e5483e316",
+    "url": "/deploy-reactjs/static/js/main.07cc854d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/deploy-reactjs/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "07cc854dca6e5483e316",
+    "url": "/deploy-reactjs/static/css/main.112aeee9.chunk.css"
+  },
+  {
+    "revision": "689ea957c12cd1564dcbb11304198284",
+    "url": "/deploy-reactjs/index.html"
+  }
+];
